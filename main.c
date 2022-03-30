360c516a5b751a87c52597aaffc4723fe583745d
cvbn760 <ross31456@gmail.com>
2022-03-30 13:34:51 +0300

    добавлена структура дерево

--- main.c
+++ main.c
@@ -1,6 +1,20 @@
 #include <stdio.h>
+#include "tree.h"
 
 int main() {
-    printf("Hello, World!\n");
+    Tree *tree = new_tree(DECIMAL_ELEM, STRING_ELEM);
+
+    set_tree(tree, decimal(50), string("A"));
+    set_tree(tree, decimal(40), string("B"));
+
+//    del_tree(tree, decimal(40));
+
+    int64_t x = 15;
+    if (in_tree(tree, decimal(x))) {
+        printf("%lf\n", get_tree(tree, decimal(x)).real);
+    }
+
+    print_tree_as_list(tree);
+    free_tree(tree);
     return 0;
 }
