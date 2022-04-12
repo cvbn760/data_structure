c138bc7246d59ca5ac095bce16fb129c21df2f0a
cvbn760 <ross31456@gmail.com>
2022-04-12 13:47:13 +0300

    clear

---
+++ cmake-build-debug/CMakeFiles/data_structure.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/data_structure.dir/main.c.obj"
+  "CMakeFiles/data_structure.dir/main.c.obj.d"
+  "data_structure.exe"
+  "data_structure.exe.manifest"
+  "data_structure.pdb"
+  "libdata_structure.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/data_structure.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
